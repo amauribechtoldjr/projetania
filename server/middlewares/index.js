917b37142d835f355d662dd4a73e05793b67c89a
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2020-11-02 18:44:23 -0300

    Adicionandno configurações para subir para produção

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,22 +1,30 @@
-const session = require('express-session');
-const config = require('../config/dev');
-const passport = require('passport');
+const session = require("express-session");
+const config = require("../config");
+const passport = require("passport");
 
 exports.init = (server, db) => {
-  require('./passport').init(passport);
+  require("./passport").init(passport);
 
   const sess = {
-    name: 'projetania-session',
+    name: "projetania-session",
     secret: config.SESSION_SECRET,
     cookie: { maxAge: 2 * 60 * 60 * 1000 },
     resave: false,
     saveUninitialized: false,
-    store: db.initSessionStore()
+    store: db.initSessionStore(),
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    server.set("trus proxy", 1);
+    sess.cookie.secure = true;
+    sess.cookie.httpOnly = true;
+    sess.cookie.sameSite = true;
+    sess.cookie.domain = process.env.DOMAIN;
   }
 
   server.use(session(sess));
   server.use(passport.initialize());
   server.use(passport.session());
-}
+};
 
-exports
+exports;
